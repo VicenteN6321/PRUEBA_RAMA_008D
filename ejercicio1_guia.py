6ccac74600716171b462beee06a2d2fb4bb6f852
VicenteN6321 <vi.navarron@duocuc.cl>
2025-06-03 12:42:01 -0400

    Delete ejercicio1_guia.py

--- ejercicio1_guia.py
+++
@@ -1,8 +0,0 @@
-materia = ['matematicas','fisica','quimica','historia','lenguaje']
-mat_rep = []
-
-for  mat in  materia:
-    nota = float(input(f"Ingrese el promedio final de {mat} "))
-    if nota < 4:
-        mat_rep.append(mat)
-print(mat_rep)         
